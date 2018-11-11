14c56d7fd067a430756a4b38d4ff42c4b0169eab
Piyush <piyushsingh90@outlook.in>
2018-11-11 18:00:14 +0530

    user verification page enhancement

--- src/app/admin/verify/verify.component.ts
+++ src/app/admin/verify/verify.component.ts
@@ -2,29 +2,35 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { UserService } from '../../user.service';
 import 'rxjs/add/operator/take';
+import { AlertService } from '../../alert.service';
 
 @Component({
   selector: 'app-verify',
   templateUrl: './verify.component.html',
   styleUrls: ['./verify.component.css']
 })
-export class VerifyComponent implements OnInit {
+export class VerifyComponent {
   user = {};
   id;
 
   constructor(private router: Router,
     private route: ActivatedRoute,
-    private userService: UserService) {
+    private userService: UserService,
+    private alertService: AlertService) {
     this.id = this.route.snapshot.paramMap.get('id');
     this.userService.get(this.id).take(1).subscribe(u => this.user = u);
-  }
-
-  ngOnInit() {
   }
 
   save(user) {
     if (this.id) {
       this.userService.update(this.id, user);
+      let message;
+      if (user.isVerified) {
+        message = user.userName + ' has been verified';
+      } else {
+        message = user.userName + ' has been un-verified';
+      }
+      this.alertService.success(message, true);
       this.router.navigate(['/admin/manage']);
     } else {
       this.userService.save(user);
